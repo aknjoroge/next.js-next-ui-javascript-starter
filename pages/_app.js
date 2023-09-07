d12bdcab693bdc2b02d70a00d3504c0c7a0f2b48
aknjoroge <alexkaranja629@gmail.com>
2023-09-07 10:00:00 +0300

    removed starter ts

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import "@/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { fontSans, fontMono } from "@/config/fonts";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider>
+        <Component {...pageProps} />
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
+}
+
+export const fonts = {
+  sans: fontSans.style.fontFamily,
+  mono: fontMono.style.fontFamily,
+};
